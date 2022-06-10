b5e17e65b5eca8837ac15675aea5eef0a962f798
will henrique <willhc1984@gmail.com>
2022-06-10 09:27:03 -0300

    Exercicios generics

---
+++ src/generics2/CalculationService.java
@@ -0,0 +1,22 @@
+package generics2;
+
+import java.util.List;
+
+public class CalculationService {
+	
+	public static Integer max(List<Integer> list) {
+		
+		if(list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty.");
+		}
+		Integer max = list.get(0);
+		for(Integer item : list) {
+			if(item.compareTo(max) > 0) {
+				max = item;
+			}
+		}
+		
+		return max;
+	}
+
+}
---
+++ src/generics2/CalculationService2.java
@@ -0,0 +1,22 @@
+package generics2;
+
+import java.util.List;
+
+public class CalculationService2 {
+	
+	public static <T extends Comparable<T>> T max(List<T> list) {
+		
+		if(list.isEmpty()) {
+			throw new IllegalStateException("List can't be empty.");
+		}
+		T max = list.get(0);
+		for(T item : list) {
+			if(item.compareTo(max) > 0) {
+				max = item;
+			}
+		}
+		
+		return max;
+	}
+
+}
